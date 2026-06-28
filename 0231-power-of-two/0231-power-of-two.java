1a54524a0615ebcdee194595f10faca52d8c7654
Rajat Ramakrishna Bhat <148202291+Rj2057@users.noreply.github.com>
2026-06-28 17:56:35 +0530

    Time: 1 ms (96.48%), Space: 42.8 MB (5.07%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-       if(n >= Integer.MIN_VALUE && n<= Integer.MAX_VALUE){
         return (n>0 && (n & (n-1)) == 0);
-       } 
-       return true;
+
     }
 }
