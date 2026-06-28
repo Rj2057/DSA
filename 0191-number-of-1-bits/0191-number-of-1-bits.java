f5b142dcedbad962bd19b993b975b869b052c3aa
Rajat Ramakrishna Bhat <148202291+Rj2057@users.noreply.github.com>
2026-06-28 17:39:29 +0530

    Time: 0 ms (100%), Space: 42.5 MB (28.13%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingWeight(int n) {
+       if(n >= 1 && n <= Integer.MAX_VALUE){
+          int result = 0;
+          while(n >0){
+            result += (n&1);
+            n>>=1;   
+          }
+          return result; 
+        }  
+    return 0;  
+  }
+  
+}
